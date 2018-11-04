95f7134611de0408eb6248bcd1e39e8ea6f26e80
carolynwang <carolyn.wang@berkeley.edu>
2018-11-03 18:47:04 -0700

    log in

--- test.js
+++ test.js
@@ -1,15 +1,68 @@
 const client = stitch.Stitch.initializeDefaultAppClient('tutormatch-fxrqk');
 
+const APP_ID = "<YOUR APP ID>";
+const {
+  Stitch,
+  UserPasswordCredential,
+} = stitch;
+const stitchClient = Stitch.initializeDefaultAppClient(APP_ID);
+
 const db = client.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-atlas').db('tutorDB');
 
-client.auth.loginWithCredential(new stitch.AnonymousCredential()).then(user => 
+if (stitchClient.auth.isLoggedIn) {
+  hideLoginForm();
+  revealDashboardContainer();
+  build(Date.now());
+}
+
+async function handleLogin() {
+  const { email, password } = getLoginFormInfo();
+  await emailPasswordAuth(email, password);
+  build(Date.now());
+}
+
+async function emailPasswordAuth(email, password) {
+  if (!stitchClient.auth.isLoggedIn) {
+    // Log the user in
+    const credential = new UserPasswordCredential(email, password);
+    await stitchClient.auth.loginWithCredential(credential);
+  }
+  hideLoginForm();
+  revealDashboardContainer();
+}
+
+/*client.auth.loginWithCredential(new stitch.AnonymousCredential()).then(user =>
   db.collection('users').updateOne({owner_id: client.auth.user.id}, {$set:{number:42}}, {upsert:true})
-).then(() => 
+).then(() =>
   db.collection('users').find({owner_id: client.auth.user.id}, { limit: 100}).asArray()
 ).then(docs => {
   console.log("Found docs", docs)
   console.log("[MongoDB Stitch] Connected to Stitch")
 }).catch(err => {
   console.error(err)
-});
+});*/
 
+/* UI Management Functions */
+function getLoginFormInfo() {
+  const emailEl = document.getElementById("emailInput");
+  const passwordEl = document.getElementById("passwordInput");
+  // Parse out input text
+  const email = emailEl.value;
+  const password = passwordEl.value;
+  // Remove text from login boxes
+  emailEl.value = "";
+  passwordEl.value = "";
+  return { email: email, password: password };
+}
+
+function hideLoginForm() {
+  const user = stitchClient.auth.user;
+  loginForm.classList.add("hidden");
+  // Set login status message
+  statusMessage.innerText = "Logged in as: " + user.profile.data.email;
+};
+
+function revealDashboardContainer() {
+  const container = document.getElementById("dashboard-container");
+  container.classList.remove("hidden");
+}
